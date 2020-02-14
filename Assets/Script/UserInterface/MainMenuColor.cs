8cd66a49c341222ef557e135704174d2b0fddfbc
Chieh Wen Chen <sherwinnie@Chiehs-MacBook-Pro.local>
2020-02-13 22:37:29 -0500

    main menu in progress

--- Assets/Script/UserInterface/MainMenuColor.cs
+++ Assets/Script/UserInterface/MainMenuColor.cs
@@ -6,25 +6,24 @@ public class MainMenuColor : MonoBehaviour
 {
     private Dictionary<char, Color> CubeColor;
     private Dictionary<char, Color> BackgroundColor;
-    private Dictionary<char, Color> PlatformColor;
+    private Dictionary<char, Color> TextColor;
     
     void Start()
     {
         CubeColor = new Dictionary<char, Color>
-        {
-            { ''},
-            { },
-            { },
-            { },
+        { 
         };
         BackgroundColor = ColorHolder.BackgroundColor;
-        PlatformColor = ColorHolder.PlatformColor;
+        CubeColor = ColorHolder.PlatformColor;
+        TextColor = ColorHolder.PlayerColor;
 
         char[] AllColor = { 'g', 'l', 'r', 'c' };
         char ColorChar = AllColor[Random.Range(0, AllColor.Length)];
         Debug.Log(ColorChar);
         
-        GameObject.Find("Camera").GetComponent<Camera>().backgroundColor = BackgroundColor[ColorChar];
-        GameObject.Find("Cube").GetComponent<Renderer>().material.color = PlatformColor[ColorChar];
+        GameObject.Find("Camera").GetComponent<Camera>().backgroundColor = BackgroundColor['l'];
+        GameObject.Find("Cube").GetComponent<Renderer>().material.color = CubeColor['l'];
+
+
     }
 }
